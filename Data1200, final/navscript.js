833df57ca6882c064db92c9ca258ae0e6d4738bd
s365334 <89581606+s365334@users.noreply.github.com>
2021-11-25 12:36:52 +0100

    endirnger

--- Data1200, final/navscript.js
+++ Data1200, final/navscript.js
@@ -1,3 +1,6 @@
+/* https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+Our source, to make it responsive and interactive. Making it so that we can click the icon to switch between topnav,
+and topnav.responsive */
 function navigation(){
     var x = document.getElementById("Nav-bar");
     if (x.className === "topnav"){
@@ -8,6 +11,10 @@ function navigation(){
     }
 }
 
+
+/* https://www.w3schools.com/howto/howto_css_menu_icon.asp
+Our source, to make sure that you can toggle between the menu.
+Making it animated, and causing it to rotate.*/
 function navigation2(x) {
     x.classList.toggle("change");
 }
--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 [class*="col-"] {
   width: 100%;
 }
+
+Source material:
+We used W3 schools' tutorial to help create our nav bar to make it responsive, with a more intriguing design.
+https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+https://www.w3schools.com/howto/howto_css_menu_icon.asp 
