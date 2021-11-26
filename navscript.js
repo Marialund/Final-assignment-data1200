9ef6c330c9722adc4030df7788e44e4d9174e877
Belsaas <aasmund.belsaas@hotmail.com>
2021-11-26 14:46:28 +0100

    Update navscript.js

--- navscript.js
+++ navscript.js
@@ -23,28 +23,32 @@ function navigation2(x) {
 
 /* Slideshow */
 
-// Slide-index starts at 0
-var slideIndex = 0;
+// Slideshow-index starts at 0
+var memeIndex = 0;
 
 // Main function uses above variable as parameter   
-beginMemeSlideshow(slideIndex);
+beginMemeSlideshow(memeIndex);
 
 // Button for next meme - function initiates through onclick
 function nextSlide(x) {
-  beginMemeSlideshow(slideIndex += x);
+  beginMemeSlideshow(memeIndex += x);
 }
 
 // Main function starts slideshow
 function beginMemeSlideshow(x) {
-  var i;
+  var y;
+
+  // Imports pictures from HTML
   var slides = document.getElementsByClassName("memeSlides");
 
-//If  
-  if (x > slides.length) {slideIndex = 1}
-  if (x < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  
-  slides[slideIndex-1].style.display = "block";
+    // Makes use of the inbuilt JavaScript index measuring method ".length".
+    // If the index number (x) is bigger than the index's length, 
+    // the function resets the index counter to the beginning.
+        if (x > slides.length) {memeIndex = 1}
+    
+        if (x < 1) {memeIndex = slides.length}
+        for (y = 0; y < slides.length; y++) {
+            slides[y].style.display = "none";
+        }
+  slides[memeIndex-1].style.display = "block";
 }
