45bfa959fdffd7b7b897bb8e2dc0e30a6333a45a
Belsaas <aasmund.belsaas@hotmail.com>
2021-11-26 12:31:05 +0100

    k

--- slideshow.js
+++ slideshow.js
@@ -1,24 +1,21 @@
 var slideIndex = 0;
-showSlides(slideIndex);
+startMemeSlideshow(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+
+// Button for next meme
+function nextSlide(n) {
+  startMemeSlideshow(slideIndex += n);
 }
 
 
-function showSlides(n) {
+function startMemeSlideshow(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("memeSlides");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
